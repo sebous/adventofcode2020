18e3cfb8177f460ba33035114525dbd041634c60
sebous <sebous@gmail.com>
2020-12-01 12:25:52 +0100

    up

--- src/solutions/01/ts/01.ts
+++ src/solutions/01/ts/01.ts
@@ -1,7 +1,7 @@
 import path from "path";
 import { readInputFile } from "../../../lib/input";
 
-async function part01() {
+async function run() {
   const input = await readInputFile(path.join(__dirname, "../data.txt"));
   const data = input.split("\n").map(Number);
 
@@ -10,16 +10,7 @@ async function part01() {
       if (data[x] + data[y] === 2020) {
         console.log(`Part 1: ${data[x] * data[y]}`);
       }
-    }
-  }
-}
 
-async function part02() {
-  const input = await readInputFile(path.join(__dirname, "../data.txt"));
-  const data = input.split("\n").map(Number);
-
-  for (let x = 0; x < data.length; x++) {
-    for (let y = x + 1; y < data.length; y++) {
       for (let z = y + 1; z < data.length; z++) {
         if (data[x] + data[y] + data[z] === 2020) {
           console.log(`Part 2: ${data[x] * data[y] * data[z]}`);
@@ -29,5 +20,4 @@ async function part02() {
   }
 }
 
-part01();
-part02();
+run();
