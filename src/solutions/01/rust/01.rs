c4be6029bfcfc75ab6d5c0ded14698947264abbf
sebous <sebous@gmail.com>
2020-12-01 11:58:49 +0100

    day01 in rust

--- src/solutions/01/rust/01.rs
+++ src/solutions/01/rust/01.rs
@@ -1,9 +1,20 @@
-// use crate::lib::input;
-use std::fs;
-use std::path::PathBuf;
+use crate::lib::input;
 
 pub fn day01() {
-    let a = PathBuf::from("./solutions/01/data.txt");
-    println!("{:?}", a);
-    // input::load_input("../data.txt");
+    let input_data = input::load_input("src/solutions/01/data.txt");
+    let data = input_data.lines().map(|l| l.parse().unwrap()).collect::<Vec<u32>>();
+
+    for x in 0..data.len() {
+        for y in x + 1..data.len() {
+            if data[x] + data[y] == 2020 {
+                println!("Part 1: {}", data[x] * data[y]);
+            }
+
+            for z in y + 1..data.len() {
+                if data[x] + data[y] + data[z] == 2020 {
+                    println!("Part 2: {}", data[x] * data[y] * data[z]);
+                }
+            }
+        }
+    }
 }
