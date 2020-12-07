3e54ae162145d209f7b99fe90f2bb440cf3de9e6
sebous <sebous@gmail.com>
2020-12-07 10:59:14 +0100

    removed all days ts call

--- src/advent.ts
+++
@@ -1,9 +0,0 @@
-(async () => {
-  const date = new Date().getDate();
-
-  for (let x = 1; x <= date; x++) {
-    const datePadded = x < 10 ? `0${x}` : x;
-    console.log(`Day ${datePadded}:`);
-    await import(`./solutions/${datePadded}/ts/${datePadded}`);
-  }
-})();
