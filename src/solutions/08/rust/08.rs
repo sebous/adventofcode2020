4cc3d22804f4a857a009845d54d7c27d0c52acc9
sebous <sebous@gmail.com>
2020-12-08 13:00:40 +0100

    cleanup

--- src/solutions/08/rust/08.rs
+++ src/solutions/08/rust/08.rs
@@ -24,19 +24,21 @@ fn parse_input(input: &String) -> Program {
         .collect();
     return program;
 }
-
-fn run_till_repeat(program: &mut Program) -> (bool, i32) {
+/// returns (true if program ends by infinite loop, accumulator)
+fn run_till_end(program: &mut Program, logging: bool) -> (bool, i32) {
     let mut pointer: usize = 0;
     let mut accumulator: i32 = 0;
 
     loop {
-        if pointer > program.len() - 1 {
+        if pointer >= program.len() {
             return (false, accumulator);
         }
-        // println!(
-        //     "instr: {:?}, pointer: {}, acc: {}",
-        //     program[pointer], pointer, accumulator
-        // );
+        if logging {
+            println!(
+                "instr: {:?}, pointer: {}, acc: {}",
+                program[pointer], pointer, accumulator
+            );
+        }
         if program[pointer].ran {
             return (true, accumulator);
         } else {
@@ -65,7 +67,7 @@ fn try_instr_change(program: &Program) -> i32 {
             continue;
         }
 
-        let (stuck, accumulator) = run_till_repeat(&mut test_program);
+        let (stuck, accumulator) = run_till_end(&mut test_program, false);
         if !stuck {
             return accumulator;
         }
@@ -76,7 +78,7 @@ fn try_instr_change(program: &Program) -> i32 {
 pub fn run() {
     let input = load_input("src/solutions/08/data.txt");
     let mut program: Program = parse_input(&input);
-    let (_, part1) = run_till_repeat(&mut program.clone());
+    let (_, part1) = run_till_end(&mut program.clone(), false);
     println!("Part 1: {:?}", part1);
 
     let part2 = try_instr_change(&mut program);
