3802cad10bcc3d5e9c0b0c67cee11a7fd012a3cd
sebous <sebous@gmail.com>
2020-12-11 16:31:28 +0100

    day 11 p2

--- src/solutions/11/rust/11.rs
+++ src/solutions/11/rust/11.rs
@@ -1,6 +1,5 @@
-use std::collections::HashMap;
-
 use crate::lib::input::load_input;
+use std::collections::HashMap;
 
 type Coord = (usize, usize);
 type GridInfo = (HashMap<Coord, Place>, usize, usize);
@@ -71,7 +70,7 @@ fn get_adjacent_coords(
     return adj_coords;
 }
 
-fn process_seating_layout(grid_info: &GridInfo) {
+fn process_seating_layout(grid_info: &GridInfo, directional: bool) -> usize {
     let (source_grid, width, height) = grid_info;
     let mut grid = source_grid.clone();
     let mut next_grid = source_grid.clone();
@@ -84,25 +83,28 @@ fn process_seating_layout(grid_info: &GridInfo) {
             for y in 0..*height + 1 {
                 let coord = (x, y);
                 if let Some(place) = grid.get(&coord) {
-                    if *place == Place::FLOOR {
+                    if *place == Place::FLOOR || *place == Place::OCC {
                         continue;
                     }
                 }
 
                 let mut occupied = 0;
-                for c in get_adjacent_coords(&grid_info, &coord, &mut cache) {
-                    if let Some(place) = grid.get(&c) {
-                        if *place == Place::OCC {
-                            occupied += 1;
+                if !directional {
+                    for c in get_adjacent_coords(&grid_info, &coord, &mut cache) {
+                        if let Some(place) = grid.get(&c) {
+                            if *place == Place::OCC {
+                                occupied += 1;
+                            }
                         }
                     }
+                } else {
+                    occupied += count_visible_occupied(&grid, &coord);
                 }
+
                 if occupied == 0 {
                     if let Some(place) = next_grid.get_mut(&coord) {
-                        if *place == Place::EMPTY {
-                            *place = Place::OCC;
-                            changes += 1;
-                        }
+                        *place = Place::OCC;
+                        changes += 1;
                     }
                 }
             }
@@ -115,26 +117,30 @@ fn process_seating_layout(grid_info: &GridInfo) {
             for y in 0..*height + 1 {
                 let coord = (x, y);
                 if let Some(place) = grid.get(&coord) {
-                    if *place == Place::FLOOR {
+                    if *place == Place::FLOOR || *place == Place::EMPTY {
                         continue;
                     }
                 }
 
                 let mut occupied = 0;
-                for c in get_adjacent_coords(&grid_info, &coord, &mut cache) {
-                    if let Some(place) = grid.get(&c) {
-                        if *place == Place::OCC {
-                            occupied += 1;
+                if !directional {
+                    for c in get_adjacent_coords(&grid_info, &coord, &mut cache) {
+                        if let Some(place) = grid.get(&c) {
+                            if *place == Place::OCC {
+                                occupied += 1;
+                            }
                         }
                     }
+                } else {
+                    occupied += count_visible_occupied(&grid, &coord);
                 }
 
-                if occupied >= 4 {
+                let occ_empty_threshold = if directional { 5 } else { 4 };
+
+                if occupied >= occ_empty_threshold {
                     if let Some(place) = next_grid.get_mut(&coord) {
-                        if *place == Place::OCC {
-                            *place = Place::EMPTY;
-                            changes += 1;
-                        }
+                        *place = Place::EMPTY;
+                        changes += 1;
                     }
                 }
             }
@@ -152,7 +158,7 @@ fn process_seating_layout(grid_info: &GridInfo) {
         .filter(|(_, place)| **place == Place::OCC)
         .count();
 
-    println!("Part 1: {}", occupied_seats);
+    return occupied_seats;
 }
 
 const DIRECTIONS: [(isize, isize); 8] = [
@@ -165,10 +171,41 @@ const DIRECTIONS: [(isize, isize); 8] = [
     (0, -1),
     (1, -1),
 ];
-fn count_visible(grid_info: &GridInfo, coord: &Coord, cache: &mut HashMap<Coord, Vec<Coord>>) {}
+fn count_visible_occupied(grid: &HashMap<Coord, Place>, coord: &Coord) -> u32 {
+    let mut occupied = 0;
+    let (curr_x, curr_y) = *coord;
+
+    for d in DIRECTIONS.iter() {
+        let (move_x, move_y) = *d;
+        let mut prev = (curr_x, curr_y);
+        loop {
+            let (prev_x, prev_y) = prev;
+            let next = (
+                (prev_x as isize + move_x) as usize,
+                (prev_y as isize + move_y) as usize,
+            );
+            if let Some(p) = grid.get(&next) {
+                if *p == Place::OCC {
+                    occupied += 1;
+                    break;
+                } else if *p == Place::EMPTY {
+                    break;
+                }
+            } else {
+                break;
+            }
+            prev = next.clone()
+        }
+    }
+    return occupied;
+}
 
 pub fn run() {
     let input = load_input("src/solutions/11/data.txt");
     let mut grid_info: GridInfo = parse_input(&input);
-    process_seating_layout(&grid_info);
+    let part1 = process_seating_layout(&grid_info, false);
+    println!("Part 1: {}", part1);
+
+    let part2 = process_seating_layout(&grid_info, true);
+    println!("Part 2: {}", part2);
 }
