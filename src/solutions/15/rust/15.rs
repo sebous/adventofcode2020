709095bd749ab2b6d7af34a2bcbc0ff1e49081de
sebous <sebous@gmail.com>
2020-12-15 11:30:34 +0100

    cleanup

--- src/solutions/15/rust/15.rs
+++ src/solutions/15/rust/15.rs
@@ -3,30 +3,28 @@ use std::collections::HashMap;
 fn calculate_nth_turn(input: &Vec<u64>, n: u64) {
     let mut last: u64 = 0;
     let mut turn: u64 = 1;
-    let mut number_turn: HashMap<u64, (u64, u64)> = HashMap::new();
+    let mut cache: HashMap<u64, (u64, u64)> = HashMap::new();
 
     loop {
         if (turn as usize) - 1 < input.len() {
             let i = (turn as usize) - 1;
             last = input[i];
         } else {
-            if let Some(turns) = number_turn.get(&last) {
-                let (t1, t2) = turns;
-                if *t2 == 0 {
-                    last = 0;
-                } else {
-                    last = *t1 - *t2;
-                }
+            let (t1, t2) = cache.get(&last).unwrap();
+            if *t2 == 0 {
+                last = 0;
+            } else {
+                last = *t1 - *t2;
             }
         }
 
         // println!("{}, {}", turn, last);
-        if let Some(turns) = number_turn.get_mut(&last) {
+        if let Some(turns) = cache.get_mut(&last) {
             let (t1, t2) = turns;
             *t2 = *t1;
             *t1 = turn;
         } else {
-            number_turn.insert(last, (turn, 0));
+            cache.insert(last, (turn, 0));
         }
 
         turn += 1;
